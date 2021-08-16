7f8595726a93ca0db7735a05b4021c4ebe099080
Joseph C. Hardin <joseph@climate.ai>
2021-08-16 18:50:05 +0000

    Added cloning of two climateAI directories, data science base and NSF proejct

--- setup_vm.sh
+++ setup_vm.sh
@@ -3,3 +3,10 @@
 echo "Setting up climateAI project directory in ~/github.climate.ai"
 mkdir -p $HOME/github.climate.ai
 
+echo "Setting git username/email"
+git config --global user.email "joseph@climate.ai"
+git config --global user.name "Joseph C. Hardin"
+
+cd $HOME/github.climate.ai
+git clone git@github.com:ClimateAI/data-science-base.git 
+git clone git@github.com:ClimateAI/nsf-phase1.git
